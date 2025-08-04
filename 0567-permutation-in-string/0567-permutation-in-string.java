284534ca7b5118e1cbea9f039fcea653b078d7b4
vikkired-14 <30293327+vikkired-14@users.noreply.github.com>
2025-08-04 14:17:46 +0530

    Time: 8 ms (60.43%), Space: 42.6 MB (79.45%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,37 +2,35 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length()>s2.length())
           return false;
-        int[] s1Arr = new int[26];
-        int[] s2Arr = new int[26];
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];  
         for(int i=0;i<s1.length();i++){
-            s1Arr[s1.charAt(i)-'a']++;
-            s2Arr[s2.charAt(i)-'a']++;
+            s1arr[s1.charAt(i)-'a']++;
+            s2arr[s2.charAt(i)-'a']++;
         }
-
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            if (s1Arr[i] == s2Arr[i])
-                count++;
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(s1arr[i]==s2arr[i])
+             count++;
         }
-
+        if(count==26)
+          return true;
         for(int i=0;i<s2.length()-s1.length();i++){
+            int r = s2.charAt(i+s1.length())-'a';
+            int l = s2.charAt(i)-'a';
             if(count==26)
               return true;
-            int r = s2.charAt(i+s1.length())-'a';
-            int l =   s2.charAt(i)-'a';
-            s2Arr[r]++;
-            if(s2Arr[r] == s1Arr[r]){
-                count++;
-            }else if(s2Arr[r] == s1Arr[r]+1){
-                count--;
-            }
-             s2Arr[l]--;
-             if(s2Arr[l] == s1Arr[l])
-               count++;
-             else if(s2Arr[l] == s1Arr[l]-1)  
-               count--;
-        }
-
-        return count==26;
+            s2arr[r]++;
+            if(s2arr[r] == s1arr[r])
+              count++;
+            else if(s2arr[r] == s1arr[r]+1)
+             count--;
+            s2arr[l]--; 
+            if(s2arr[l] == s1arr[l])
+              count++;
+            else if(s2arr[l] == s1arr[l]-1)
+             count--;    
+        }  
+       return count==26; 
     }
 }
