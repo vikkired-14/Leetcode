457bff18eeb850fad3caa7f32db6038226bcc347
vikkired-14 <30293327+vikkired-14@users.noreply.github.com>
2025-08-05 12:15:57 +0530

    Time: 69 ms (5.66%), Space: 57.6 MB (66.36%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,29 +1,23 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-       int left =0;
        Map<Integer,Integer> cache = new HashMap<>();
-       long sum =0;
-       long maxSum = 0;
-      for(int right=0;right<nums.length;right++){
-        while(cache.containsKey(nums[right])){
+       int left=0;
+       long sum=0;
+       long maxSum =0;
+       for(int right=0;right<nums.length;right++){
+          cache.put(nums[right],cache.getOrDefault(nums[right],0)+1);
+          sum += nums[right];
+          while(right-left+1>k || cache.get(nums[right])>1){
+            cache.put(nums[left],cache.get(nums[left])-1); 
+            if(cache.get(nums[left])==0)
+              cache.remove(nums[left]);  
             sum -= nums[left];
-            cache.remove(nums[left++]);
-        }
-        cache.put(nums[right],right);
-        if(cache.size()>k){
-             sum -= nums[left];
-            cache.remove(nums[left++]);
-        }
-         sum += nums[right];
-         
-         if(cache.size()==k){
-           maxSum = Math.max(maxSum,sum);
-         }
-          
-        
-         
-      } 
-
-      return maxSum;
+            left++;  
+          }
+          if(right-left+1==k){
+            maxSum = Math.max(maxSum,sum);
+          }
+       } 
+       return maxSum;
     }
 }
