a4c245cd3be648350ad97b1881b082cd8463898b
vikkired-14 <30293327+vikkired-14@users.noreply.github.com>
2025-08-16 16:13:13 +0530

    Time: 3 ms (99.78%), Space: 46.2 MB (82.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int min =Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         for(int n: nums){
             min = Math.min(min,n);
             max = Math.max(max,n);
         }
         int[] freq = new int[max-min+1];
-        for(int n: nums){
-            freq[n-min]++;
+        for(int n:nums){
+          freq[n-min]++;
         }
         List<Integer>[] bucket = new List[nums.length+1];
         for(int i=0;i<freq.length;i++){
             if(freq[i]>0){
-                if(bucket[freq[i]]==null)
-                 bucket[freq[i]]=new ArrayList<>(); 
-                 bucket[freq[i]].add(i+min);
+                if(bucket[freq[i]]==null)   
+                   bucket[freq[i]] = new ArrayList<>();
+                bucket[freq[i]].add(i+min);   
             }
         }
+
         int[] res = new int[k];
         int idx=0;
-      for(int i = bucket.length-1;i>=0 && idx<k;i--){
-        if(bucket[i]!=null){
-          for(int j : bucket[i]){
-            res[idx++] =j;
-             if (idx == k) break;
-          }
+        for(int i= bucket.length-1;i>=0 && idx<k;i--){
+           if(bucket[i]!=null){
+            for(int j : bucket[i]){
+                res[idx++] = j;
+                if(idx==k) break ;
+            }
+           }
         }
-          
-      } 
-
-      return res; 
+        return res;
     }
 }
