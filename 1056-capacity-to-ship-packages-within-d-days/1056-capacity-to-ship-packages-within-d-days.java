8bcfbf1fbeb0117385b35d61c62ea35e10721a1b
vikkired-14 <30293327+vikkired-14@users.noreply.github.com>
2025-09-04 14:25:07 +0530

    Time: 11 ms (77.2%), Space: 47.1 MB (85.13%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        
+        int left = 0;
+        int right =0;
+        for(int wt: weights){
+            left = Math.max(wt,left);
+            right += wt;
+        }
+        // System.out.println(left +":" + right);
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(canShip(weights,days,mid)){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+
+    public boolean canShip(int[] weights,int days,int capacity){
+        int count =1;
+        int currLoad=0;
+        for(int wt: weights){
+           if(currLoad+wt>capacity){
+             count++;
+             currLoad =0;
+           }
+           currLoad += wt;
+           if(count>days) return false;
+
+        }
+        return count<=days;
+    }
+}
