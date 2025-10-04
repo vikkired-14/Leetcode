08e695424a5bd56466fbc24de665359eb123dfc5
vikkired-14 <30293327+vikkired-14@users.noreply.github.com>
2025-10-04 17:11:25 +0530

    Time: 3 ms (99.7%), Space: 45.7 MB (88.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -3,32 +3,32 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         for(int n: nums){
-            min = Math.min(min,n);
-            max = Math.max(max,n);
+            min = Math.min(n,min);
+            max = Math.max(n,max);
         }
-        int[] freq = new int[max-min+1];
-        for(int n:nums){
-          freq[n-min]++;
+        int[] count = new int[max-min+1];
+        for(int n : nums){
+            count[n-min]++;
         }
         List<Integer>[] bucket = new List[nums.length+1];
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]>0){
-                if(bucket[freq[i]]==null)   
-                   bucket[freq[i]] = new ArrayList<>();
-                bucket[freq[i]].add(i+min);   
+        for(int i=0;i<count.length;i++){
+            if(count[i]>0){
+            if(bucket[count[i]]==null)
+                bucket[count[i]] = new ArrayList<>();
+             bucket[count[i]].add(i+min); 
             }
         }
-
-        int[] res = new int[k];
         int idx=0;
-        for(int i= bucket.length-1;i>=0 && idx<k;i--){
-           if(bucket[i]!=null){
-            for(int j : bucket[i]){
-                res[idx++] = j;
-                if(idx==k) break ;
+        int[] res = new int[k];
+        for(int i = bucket.length-1;i>=0;i--){
+            if(bucket[i]!=null){
+                for(int j: bucket[i]){
+                  if(idx<res.length)  
+                   res[idx++] = j;
+                  if(idx==k) break;
+                }
             }
-           }
         }
-        return res;
+     return res;
     }
 }
