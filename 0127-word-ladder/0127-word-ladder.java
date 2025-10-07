1d8cbfe855070ff01cb01a6dce39ede4e7ec6135
vikkired-14 <30293327+vikkired-14@users.noreply.github.com>
2025-10-07 15:31:08 +0530

    Time: 71 ms (69.15%), Space: 46.2 MB (77.34%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -2,35 +2,32 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
         Set<String> wordSet = new HashSet<>(wordList);
-        if(!wordSet.contains(endWord)){
-            return 0;
-        }
+        if(!wordSet.contains(endWord)) return 0;
         Queue<String> qu = new LinkedList<String>();
         qu.offer(beginWord);
-        int count =1;
+        int steps = 1;
         while(!qu.isEmpty()){
             int size = qu.size();
-            for(int j=0;j<size;j++){
-              String word = qu.poll();
-              char[] charArr = word.toCharArray();
-            for(int i=0;i<charArr.length;i++){
-                char ch = charArr[i];
-                for(char k = 'a';k<='z';k++){
-                    charArr[i] = k;
-                  String nextString = new String(charArr);
-                  if(endWord.equals(nextString))
-                    return count+1;
-                  if(wordSet.contains(nextString)){
-                    qu.offer(nextString);
-                    wordSet.remove(nextString);
-                  } 
+            for(int i=0;i<size;i++){
+                String word = qu.poll();
+                char[] charArr = word.toCharArray();
+                for(int j=0;j<charArr.length;j++){
+                  char ch = charArr[j];
+                  for(char c='a';c<='z';c++){
+                    if(c==ch) continue;
+                    charArr[j] = c;
+                    String newWord = new String(charArr);
+                    if(endWord.equals(newWord))
+                      return steps+1;
+                    if(wordSet.contains(newWord)){
+                        qu.offer(newWord);
+                        wordSet.remove(newWord);
+                    }  
+                  }
+                  charArr[j] = ch;
                 }
-                charArr[i] = ch;
             }
-            
-            }
-            count++;
-            
+            steps++;
         }
         return 0;
     }
